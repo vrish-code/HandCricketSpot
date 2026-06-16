7de0b4836c6d7095127998e9bf7cf762b7d43532
Vrishan Somalinga <vrishansomalinga@gmail.com>
2026-06-16 06:42:19 +0530

    resized human

--- scripts/modules/botPlay.py
+++ scripts/modules/botPlay.py
@@ -3,53 +3,82 @@ import streamlit as st
 import matplotlib as plt
 import pandas as pd
 
+st.set_page_config(layout="wide")
 
+if "playDict" not in st.session_state:
+    playDict={"Score":0,
+              "BallsPlayed":0,
+              "RunRate":0 }
 def playingInterface():
-    playDict={"Score":{"Player":0, "Bot":0},
-              "BallsPlayed":{"Player":0, "Bot":0},
-              "RunRate":{"Player":0, "Bot":0} }
+    
 
     with st.container(border=True):
-        choices=["Batting 🏏", "Bowling ⚾"]
-        choicePlay=st.pills("What do you want to play?", choices)
         choiceMatch=st.slider("How many overs for the match?", min_value=1, max_value=20, value=5)
+        choiceMatch*6
         
-        if choicePlay  and  choiceMatch:
-            choices.remove(choicePlay)
-            st.write(f"{choiceMatch} overs.")
+       
+        c1,c2=st.columns(2, border=True)  
+        with c1:
+            st.badge("Batting", color="green")
+            st.subheader(f"Player{random.randint(1000,10000)}")
+            st.divider()
+            st.image(r"images/humanIm.png.png")
+        with c2:
+            st.badge("Bowling", color="red")
+            st.subheader("Bot")
+            st.divider()
+            st.image(r"images/robotImg.png")
+            
+            
+        with st.container(border=True):
+            st.subheader("Play here.")
             c1,c2=st.columns(2, border=True)
-            
             with c1:
-             st.badge(choicePlay, color="green")
-             st.subheader(f"Player{random.randint(1000,10000)}")
-             st.divider()
-             st.image(r"images/humanIm.png.png")
-             st.divider()
-             c3,c4,c5=st.columns(3, border=True)
-             c3.metric("Score", f"{playDict["Score"]["Player"]} RUNS")
-             c4.metric("Run Rate", f"{playDict["RunRate"]["Player"]}")
-             c5.metric("Overs Played", f"{playDict["BallsPlayed"]["Player"]} OVERS")
+                st.badge("Player", color="blue")
+                st.write(f"Player to Bat")
+                runPlayed=st.slider("Choose what to play.", min_value=1, max_value=11, value=5)
+                if runPlayed:
+                    playDict["Score"]+=runPlayed
+                    playDict["BallsPlayed"]+=1
+                    playDict["RunRate"]=playDict["Score"]//playDict["BallsPlayed"]
+                  
             with c2:
-              st.badge(choices[0], color="red")
-              st.subheader("Bot")
-              st.divider()
-              st.image(r"images/robotImg.png")
-              st.divider()
-              c6,c7,c8=st.columns(3, border=True)
-              c6.metric("Score", f"{playDict["Score"]["Bot"]} RUNS")
-              c7.metric("Run Rate", f"{playDict["RunRate"]["Bot"]}")
-              c8.metric("Overs Played", f"{playDict["BallsPlayed"]["Bot"]} OVERS")\
-            
-            with st.container(border=True):
-                st.subheader("Play here.")
-                c1,c2=st.columns(2, border=True)
-                with c1:
-                  st.badge("Player", color="blue")
-                  st.write(f"Player to {choicePlay}")
-                  runPlayed=st.pills("Choose what to play.", list(range(1,11)))
-                with c2:
-                   st.badge("Bot", color="green")
-                   st.write(f"Bot to {choices[0]}")
-                   runPlayer=None
+                st.badge("Bot", color="green")
+                st.write(f"Bot to {"Bowl"}")
+                if runPlayed:
+                    runPlayedByBot=random.choice(list(range(1,11)))
+          
+            if runPlayed and runPlayedByBot:
+                with st.container(border=True):
+                    st.write(f"Player:{runPlayed}")
+                    st.write(f"Bot: {runPlayedByBot}")
+                    if runPlayed==runPlayedByBot:
+                          with c1:
+                            st.error("Out!")
+                            st.subheader("Game Stats")
+                            st.divider()
+                            with st.container(border=True):
+                                c9,c0,ca=st.columns(3, border=True)
+                                c9.metric("Score", f"{playDict["Score"]} RUNS")
+                                c0.metric("Run Rate", f"{playDict["RunRate"]}")
+                                ca.metric("Overs Played", f"{playDict["BallsPlayed"]} OVERS")
+                    elif playDict["BallsPlayed"]==choiceMatch:
+                        st.success("Match finished!")
+                        st.subheader("Game Stats")
+                        st.divider()
+                        with st.container(border=True):
+                          c9,c0,ca=st.columns(3, border=True)
+                          c9.metric("Score", f"{playDict["Score"]} RUNS")
+                          c0.metric("Run Rate", f"{playDict["RunRate"]}")
+                          ca.metric("Overs Played", f"{playDict["BallsPlayed"]} OVERS")
+                    else:
+                        st.subheader("Game Stats")
+                        st.divider()
+                        with st.container(border=True):
+                          c9,c0,ca=st.columns(3, border=True)
+                          c9.metric("Score", f"{playDict["Score"]} RUNS")
+                          c0.metric("Run Rate", f"{playDict["RunRate"]}")
+                          ca.metric("Overs Played", f"{playDict["BallsPlayed"]} OVERS")
+                    
 
 playingInterface()
