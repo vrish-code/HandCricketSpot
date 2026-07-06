3da9e238b115bc012bc280b854cbbe3ac8547cbc
Vrishan Somalinga <vrishansomalinga@gmail.com>
2026-07-06 06:37:23 +0530

    started bowling mode, need to add chat forums

--- scripts/modules/botPlayBowling.py
+++ scripts/modules/botPlayBowling.py
@@ -22,6 +22,7 @@ def startDataBase():
               "Runs Played":[],
               "Balls Played":0,
               "RunRate":0,
+              "Balls bowled":[],
                "Total Overs":overCount
                }
             st.rerun()
@@ -34,12 +35,12 @@ def playingInterface():
         with st.container(border=True):
          c1,c2=st.columns(2, border=True)  
          with c1:
-            st.badge("Batting", color="green")
+            st.badge("Bowling", color="green")
             st.subheader(st.session_state.playerName)
             st.divider()
             st.image(r"images/humanIm.png.png")
          with c2:
-            st.badge("Bowling", color="red")
+            st.badge("Batting", color="red")
             st.subheader("Bot")
             st.divider()
             st.image(r"images/robotImg.png")
@@ -56,23 +57,22 @@ def playingInterface():
             c1,c2=st.columns(2, border=True)
             with c1:
                 st.badge(st.session_state.playerName, color="blue")
-                st.write(f"Player to Bat")
-                st.warning("Please don't use this playing system after you get out or after the match is over.")
+                st.write(f"Player to Bowl")
+                st.warning("Please don't use this playing system after you got the wicket or after the match is over.")
                 runPlayed=st.slider("Choose what to play.", min_value=1, max_value=11)
                 play=st.button(f"Play {runPlayed}?")
                 if play:
-                    st.session_state.playDict["Runs Played"].append(runPlayed)
-                    st.session_state.playDict["Score"]+=runPlayed
-                    st.session_state.playDict["Balls Played"]+=1
-                    st.session_state.playDict["RunRate"]=st.session_state.playDict["Score"]//st.session_state.playDict["Balls Played"]
-                  
+                   st.session_state.playDict["Balls bowled"].append(runPlayed)
+                   st.session_state.playDict["Balls played"]-=1
+
+
             with c2:
                 st.badge("Bot", color="green")
-                st.write(f"Bot to Bowl")
+                st.write(f"Bot to Bat")
                 if play: 
                     
-                    runPlayedByBot=random.choice(list(range(1,12)))
-                    st.metric("Run played by bot", runPlayedByBot)
+                    st.session_state.runPlayedByBot=random.choice(list(range(1,12)))
+                    st.metric("Run played by bot", st.session_state.runPlayedByBot)
           
             if play and st.session_state.runPlayedByBot:
                 st.subheader("Game Stats")
@@ -81,13 +81,13 @@ def playingInterface():
                     c9,c0,ca,c=st.columns(4, border=True)
                     c9.metric("Score", f"{st.session_state.playDict["Score"]} RUNS")
                     c0.metric("Run Rate", f"{st.session_state.playDict["RunRate"]}")
-                    ca.metric("Balls Played", f"{st.session_state.playDict["Balls Played"]} BALLS")
+                    ca.metric("Balls Played", f"{st.session_state.playDict["BallsPlayed"]} BALLS")
                     c.metric("Total overs in this match", f"{st.session_state.playDict["Total Overs"]} OVERS")
                 with st.container(border=True):
                     st.write(f"Player:{runPlayed}")
-                    st.write(f"Bot: {runPlayedByBot}")
+                    st.write(f"Bot: {st.session_state.runPlayedByBot}")
                     if runPlayed==st.session_state.runPlayedByBot:
-                        st.error("Out!")
+                        st.error("You got the wicket!")
                         st.session_state.gameOver=True
                         st.subheader("Game Stats")
                         st.divider()
@@ -104,7 +104,7 @@ def playingInterface():
                             st.session_state.clear()
                             st.rerun()
                           
-                    elif st.session_state.playDict["Balls Played"]==st.session_state.playDict["Total Overs"]*6:
+                    elif st.session_state.playDict["BallsPlayed"]==st.session_state.playDict["Total Overs"]*6:
                         st.success("Match finished!")
                         st.subheader("Game Stats")
                         st.divider()
@@ -113,7 +113,7 @@ def playingInterface():
                           c9,c0,ca=st.columns(3, border=True)
                           c9.metric("Score", f"{st.session_state.playDict["Score"]} RUNS")
                           c0.metric("Run Rate", f"{st.session_state.playDict["RunRate"]}")
-                          ca.metric("Balls Played", f"{st.session_state.playDict["Balls Played"]} BALLS")
+                          ca.metric("Balls Played", f"{st.session_state.playDict["BallsPlayed"]} BALLS")
                           t.sleep(50)
                           st.session_state.clear()
                           st.rerun()   
