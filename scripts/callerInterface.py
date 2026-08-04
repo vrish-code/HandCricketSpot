5a692c4bfda84a75ee3ce7b2357d44ef5499e411
Vrishan Somalinga <vrishansomalinga@gmail.com>
2026-08-04 17:39:28 +0530

    finished handcricketspot

---
+++ scripts/callerInterface.py
@@ -0,0 +1,15 @@
+from modules import botPlayBatting as bpb
+from modules import botPlayBowling as bpbo
+import streamlit as st
+
+def userInterface():
+    st.title("Play Hand Cricket Here!")
+    st.divider()
+    t1,t2=st.tabs(["Batting (with bot)", "Bowling (with bot)"])
+    with t1:
+        bpb.playingInterface()
+    with t2:
+        bpbo.playingInterface()
+    st.divider()
+    st.info("Thank you for using my website!")
+    st.caption("Vrishan Somalinga")
